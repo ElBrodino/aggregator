0b57013491d670fb02eb34e7c576007de6605b75
Frederic Poirier <uk_jobs@hotmail.com>
2026-02-28 11:52:18 -0500

    redudant testing

--- handler_register.go
+++ handler_register.go
@@ -8,11 +8,13 @@ func handlerRegister(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("Register should only have 1 argument")
 	}
+	name := cmd.Args[0]
 
-	err := s.config.SetUser(cmd.Args[0])
+	err := s.config.SetUser(name)
 	if err != nil {
-		return fmt.Errorf("error on setting user")
+		return fmt.Errorf("error on setting user: %w", err)
 	}
 
+	fmt.Printf("User '%s' has been created and logged in.\n", name)
 	return nil
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -39,27 +39,3 @@ func TestReadWriteConfig(t *testing.T) {
 		t.Errorf("expected iser %s, got %s", testCfg.CurrentUserName, gotCfg.CurrentUserName)
 	}
 }
-
-func TestUser(t *testing.T) {
-	cfg, configPath := setupTestConfig(t)
-
-	newUserName := "new_apprentice"
-	err := cfg.SetUser(newUserName)
-	if err != nil {
-		t.Fatalf("SetUser() failed: %v", err)
-	}
-
-	cfg.CurrentUserName = newUserName
-	if cfg.CurrentUserName != newUserName {
-		t.Errorf("failed to update struct field")
-	}
-
-	savedCfg, err := readFromPath(configPath)
-	if err != nil {
-		t.Fatalf("failed to read back to config: %v", err)
-	}
-
-	if savedCfg.CurrentUserName != newUserName {
-		t.Errorf("expected saved user to be %v, got %s", newUserName, savedCfg.CurrentUserName)
-	}
-}
