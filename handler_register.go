bd7491e27e182090d8b48bb636a36a5520f33490
Frederic Poirier <uk_jobs@hotmail.com>
2026-03-01 09:10:52 -0500

    populated user with infos

--- handler_register.go
+++ handler_register.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"gator/internal/database"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func handlerRegister(s *state, cmd command) error {
+
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("Register should only have 1 argument")
 	}
-	name := cmd.Args[0]
+	newID := uuid.New()
 
-	err := s.cfg.SetUser(name)
+	name := cmd.Args[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        newID,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
 	if err != nil {
-		return fmt.Errorf("error on setting user: %w", err)
+		return fmt.Errorf("Could not create user: %w\n", err)
 	}
 
-	fmt.Printf("User '%s' has been created and logged in.\n", name)
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("User '%s' has been created but not logged in: %w\n", name, err)
+	}
+
+	fmt.Println("User createdsuccessfully!")
+	fmt.Printf("* ID:      %v\n", user.ID)
+	fmt.Printf("* Name:    %v\n", user.Name)
 	return nil
 }
